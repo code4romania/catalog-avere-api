532720162d5886db674b16beaf3fbfdf53ed2886
Lucian Ursu <lucian.ursu@gmail.com>
2016-07-25 11:10:09 +0300

    Add 'Statement' model and migration

--- avere/apps/statements/models.py
+++ avere/apps/statements/models.py
@@ -1,5 +1,20 @@
 from __future__ import unicode_literals
 
+from django.core.urlresolvers import reverse
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+
+@python_2_unicode_compatible
+class Statement(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    position = models.CharField(max_length=255)
+    position_location = models.CharField(max_length=255)
+    created = models.DateField(auto_now_add=True)
+
+    def get_absolute_url(self):
+        return reverse('statement-detail', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return '<%s %s - %s>' % (self.first_name, self.last_name, self.position)
