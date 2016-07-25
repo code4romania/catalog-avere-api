f91936b6b18bed8682b4b1d24e7189ef2bf32592
Lucian Ursu <lucian.ursu@gmail.com>
2016-07-25 11:16:39 +0300

    Add views, urls, and templates for Statement

--- avere/apps/statements/views.py
+++ avere/apps/statements/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.list import ListView
 
-# Create your views here.
+from statements.forms import StatementForm
+from statements.models import Statement
+
+
+class StatementDetailView(DetailView):
+    model = Statement
+
+
+class StatementCreateView(CreateView):
+    form_class = StatementForm
+    fields = [
+        'first_name',
+        'last_name',
+        'position',
+        'position_location'
+    ]
+
+
+class StatementUpdateView(UpdateView):
+    form_class = StatementForm
+    fields = [
+        'first_name',
+        'last_name',
+        'position',
+        'position_location'
+    ]
+
+
+class StatementListView(ListView):
+    model = Statement
