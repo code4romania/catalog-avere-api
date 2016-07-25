0936f4d78540efa5be6816e3f166c45815a34954
Lucian Ursu <lucian.ursu@gmail.com>
2016-07-25 11:18:50 +0300

    Add a static home page

--- avere/urls.py
+++ avere/urls.py
@@ -1,9 +1,10 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name='home.html')),
     url(settings.ADMIN_URL, include(admin.site.urls)),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^users/', include('users.urls', namespace='users')),
