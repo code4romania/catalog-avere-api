154f8a7bcf80c7a8cc52801b9d4cf2f3640564a5
Lucian Ursu <lucian.ursu@gmail.com>
2016-08-08 11:35:10 +0300

    Fix StatementForm declaration

--- avere/apps/statements/forms.py
+++ avere/apps/statements/forms.py
@@ -4,10 +4,11 @@ from statements.models import Statement
 
 
 class StatementForm(forms.ModelForm):
-    model = Statement
-    fields = [
-        'first_name',
-        'last_name',
-        'position',
-        'position_location',
-    ]
+    class Meta:
+        model = Statement
+        fields = [
+            'first_name',
+            'last_name',
+            'position',
+            'position_location',
+        ]
