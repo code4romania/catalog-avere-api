b32fb6ed43f00caf8b016b543f19b976e1ace254
Lucian Ursu <lucian.ursu@gmail.com>
2016-08-08 11:37:08 +0300

    Fix call to statement detail view

--- avere/apps/statements/models.py
+++ avere/apps/statements/models.py
@@ -14,7 +14,7 @@ class Statement(models.Model):
     created = models.DateField(auto_now_add=True)
 
     def get_absolute_url(self):
-        return reverse('statement-detail', kwargs={'pk': self.pk})
+        return reverse('statements:detail', kwargs={'pk': self.pk})
 
     def __str__(self):
         return '<%s %s - %s>' % (self.first_name, self.last_name, self.position)
