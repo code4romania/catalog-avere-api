1af6e04ae9c1eda453a9e97d1880e8c3a5f680fa
Lucian Ursu <lucian.ursu@gmail.com>
2016-08-24 08:55:46 +0300

    Hook in api urls and remove the normal ones

--- avere/urls.py
+++ avere/urls.py
@@ -3,12 +3,14 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
+
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='home.html')),
     url(settings.ADMIN_URL, include(admin.site.urls)),
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='index'),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^users/', include('users.urls', namespace='users')),
-    url(r'^declaratii/', include('statements.urls', namespace='statements')),
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
+    url(r'^api/statements/', include('statements.api_urls',
+                                     namespace='statements')),
 ]
