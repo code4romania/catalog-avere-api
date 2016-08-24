af7305d1b1c137879186294d7d9838df7f693c12
Lucian Ursu <lucian.ursu@gmail.com>
2016-08-24 08:50:05 +0300

    Store all statement content in a JSONField

--- avere/apps/statements/models.py
+++ avere/apps/statements/models.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from __future__ import unicode_literals
 
+from django.contrib.postgres.fields import JSONField
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
@@ -8,15 +9,10 @@ from django.utils.encoding import python_2_unicode_compatible
 
 @python_2_unicode_compatible
 class Statement(models.Model):
-    first_name = models.CharField(verbose_name='prenume', max_length=255)
-    last_name = models.CharField(verbose_name='nume', max_length=255)
-    position = models.CharField(verbose_name='funcție', max_length=255)
-    position_location = models.CharField(verbose_name='locul funcției',
-                                         max_length=255)
-    date = models.DateField(verbose_name='data declarației')
+    content = JSONField()
 
     def get_absolute_url(self):
         return reverse('statements:detail', kwargs={'pk': self.pk})
 
     def __str__(self):
-        return '<%s %s - %s>' % (self.first_name, self.last_name, self.position)
+        return '<%s>' % (self.pk,)
