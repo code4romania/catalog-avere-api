da2d37d972caa7bdddd1a696381a011a2de535b8
Lucian Ursu <lucian.ursu@gmail.com>
2016-08-25 12:40:39 +0300

    Integrate routing into the app

--- avere/frontend/src/Updates.elm
+++ avere/frontend/src/Updates.elm
@@ -1,11 +1,18 @@
 module Updates exposing (Msg(FormMsg
                            , PreviousSection
-                           , NextSection)
+                           , NextSection
+                           , ShowHome
+                           , ShowWealthStatement
+                           , ShowInterestsStatement)
                        , update)
 
 import Form exposing (Form)
+import Hop exposing (makeUrl)
+import Navigation
 
 import Models exposing (..)
+import Routing.Config
+import Routing.Utils
 
 
 type Msg
@@ -13,6 +20,14 @@ type Msg
   | FormMsg Form.Msg
   | PreviousSection
   | NextSection
+  | ShowHome
+  | ShowWealthStatement
+  | ShowInterestsStatement
+
+
+navigationCmd : String -> Cmd a
+navigationCmd path =
+  Navigation.newUrl (makeUrl Routing.Config.config path)
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
@@ -26,3 +41,18 @@ update msg ({publicServantForm} as model) =
       ({ model | currentSection = model.currentSection - 1 }, Cmd.none)
     NextSection ->
       ({ model | currentSection = model.currentSection + 1 }, Cmd.none)
+    ShowHome ->
+      let
+        path = Routing.Utils.reverse Routing.Config.HomeRoute
+      in
+        ( model, navigationCmd path)
+    ShowWealthStatement ->
+      let
+        path = Routing.Utils.reverse Routing.Config.WealthStatementRoute
+      in
+        ( model, navigationCmd path)
+    ShowInterestsStatement ->
+      let
+        path = Routing.Utils.reverse Routing.Config.InterestsStatementRoute
+      in
+        ( model, navigationCmd path)
